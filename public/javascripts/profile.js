795e254d28c1f30c2d792894c429a24ca71593d2
Gillian Chu <gillichu@gmail.com>
2017-02-19 03:45:32 -0800

    attempt1 URL linking

--- public/javascripts/profile.js
+++ public/javascripts/profile.js
@@ -1,3 +1,19 @@
+var getLocation = function(href) {
+	var l = document.createElement("a");
+	l.href = href;
+	return l;
+};
+
+var newURL = window.location.protocol + "//" + window.location.host + "/" + window.location.pathname;
+var l = getLocation(currentURL);
+console.debug(l.hostname)
+console.debug(l.pathname)
+System.out.println(l);
+
+
+// $.post('/getAnimal', {query:})
+// document.getElementById('image').sr
+
 $('.button').click(function(){
   var buttonId = $(this).attr('id');
   $('#modal-container').removeAttr('class').addClass(buttonId);
